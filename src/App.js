885e50f44215a04df604ebba7646e2b5861ea96a
codebyvik <vikasvickycoorg@gmail.com>
2021-11-09 13:02:29 +0530

    other: Initial release v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    number1: 0,
+    number2: 0,
+    operation: "+",
+    sum: 0,
+  });
+
+  const { number1, number2, operation, sum } = state;
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "number1":
+      case "number2":
+        setState({
+          ...state,
+          [name]: parseInt(value),
+        });
+        break;
+
+      case "operation":
+        setState({
+          ...state,
+          [name]: value || "",
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function calculateSum() {
+    switch (operation) {
+      case "+":
+        setState({ ...state, sum: number1 + number2 });
+        break;
+
+      case "-":
+        setState({ ...state, sum: number1 - number2 });
+        break;
+
+      default:
+        alert("Enter only '+' or '-' in operation field");
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add">
+      <label>Enter Operation </label>
+      <input
+        type="text"
+        name="operation"
+        value={operation}
+        onChange={handleInputChange}
+      />
+      <label>Enter Number </label>
+      <input
+        type="number"
+        name="number1"
+        value={number1}
+        onChange={handleInputChange}
+        required
+      />
+      <label>Enter Number </label>
+      <input
+        type="number"
+        name="number2"
+        value={number2}
+        onChange={handleInputChange}
+        required
+      />
+      <button onClick={calculateSum}>Calculate </button>
+      <label>Result : </label>
+      <input type="number" disabled value={sum} />
     </div>
   );
 }
